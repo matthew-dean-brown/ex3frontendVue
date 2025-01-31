3f0b7698528a1b0ab16bd1e79145e2f9cd68b7cb
matthew-dean-brown <mattrewbrown@gmail.com>
2025-01-31 13:06:49 +0200

    :ambulance:

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,22 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    // store data
+    products:[]
   },
-  getters: {
-  },
+  // use to change the state
   mutations: {
+    setProducts(state,payload){
+      state.products = payload
+    }
   },
+  // all your fetch/asynchronus code goes
   actions: {
+    async getProducts(context,payload){
+      let {products} = await(await fetch('http://localhost:3000/products')).json()
+      console.log(products);
+      context.commit('setProducts',products)
+    }
   },
   modules: {
   }
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
+    
     <h1>This is an about page</h1>
+    <div v-for="product in $store.state.products" :key="product.product_code">
+      {{ product.product_code }}
+      {{ product.product_name }}
+      {{ product.product_price }}
+      {{ product.product_quantity }}
+    </div>
+    {{  }}
   </div>
+
+  
 </template>
+<script>
+    export default{
+      mounted(){
+        this.$store.dispatch('getProducts')
+      }
+    }
+  </script>
