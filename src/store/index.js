1b22dbc69be95c5dbdd18820ea87242912892007
matthew-dean-brown <mattrewbrown@gmail.com>
2025-02-03 11:31:02 +0200

    update

--- src/store/index.js
+++ src/store/index.js
@@ -2,22 +2,13 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    // store data
-    products:[]
+
   },
-  // use to change the state
   mutations: {
-    setProducts(state,payload){
-      state.products = payload
-    }
+
   },
-  // all your fetch/asynchronus code goes
   actions: {
-    async getProducts(context,payload){
-      let {products} = await(await fetch('http://localhost:3000/products')).json()
-      console.log(products);
-      context.commit('setProducts',products)
-    }
+
   },
   modules: {
   }
